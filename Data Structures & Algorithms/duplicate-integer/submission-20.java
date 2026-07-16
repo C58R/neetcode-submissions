ff9066c39098ff46298e8b61e609dbec5395c009
Oscar Callejas <callejasoc1@gmail.com>
2026-07-16 15:09:17 -0400

    Add: duplicate-integer - submission-20

---
+++ Data Structures & Algorithms/duplicate-integer/submission-20.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int x : nums) {
+            if (!seen.add(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
