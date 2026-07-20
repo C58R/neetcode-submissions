9ba4deba6cadc5b83720fc044eff970c563cf0c0
Oscar Callejas <callejasoc1@gmail.com>
2026-07-20 15:05:35 -0400

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        int res = 0, max = 0;
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num,0) + 1);
+            if (count.get(num) > max) {
+                max = count.get(num);
+                res = num;
+            }
+
+        }
+        return res;
+    }
+}
