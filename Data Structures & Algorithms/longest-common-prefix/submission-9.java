b02d62e8e5941e38d56d0fc13984b57744bacfb0
Oscar Callejas <callejasoc1@gmail.com>
2026-08-08 14:42:41 -0400

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // outer loop to iterate through chars
+        for (int i = 0; i < strs[0].length(); i++) {
+
+            for (String str : strs) {
+                if (i == str.length() || str.charAt(i) != strs[0].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+
+    }
+
+}
